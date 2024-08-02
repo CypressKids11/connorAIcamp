607a4ea977a81ef6d535bb70e2ed83e9285a6ff9
CypressKids11 <134014028+CypressKids11@users.noreply.github.com>
2024-08-02 15:50:01 -0500

    Delete finalproject/nick final project/static/keys.js

--- finalproject/nick final project/static/keys.js
+++
@@ -1,7 +0,0 @@
-export const hugging_face_key = 'Bearer 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'
-export const cloudinaryCloudname = "dmm8zr0az"
-export const sightengine_api_user = "491630136"
-export const sightengine_api_secret = "C4MoxqQPspV7jVvMv728pcy2sTpjDe9A"
-export const sightengine_workflow = "wfl_g5fdiHz4maLRX7ezhd867"
-export const sightengine_listID = "tli_g5t6wWqDfOy6QW00gUBbQ"
-export const elevenlabs_api_key = "sk_b0b0f48277288d1eda62762ef1954cd1d5fb0d7fec9f22d5"
